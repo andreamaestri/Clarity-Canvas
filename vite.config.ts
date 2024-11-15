64b2dff23c1e9a689aebd04ded1647235f7b2919
Andrea <16215183+andreamaestri@users.noreply.github.com>
2024-11-15 18:57:21 +0000

    Enhance build configuration: add deploy scripts, optimize chunking, and integrate visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/[REPO_NAME]/"
+  plugins: [
+    react(),
+    visualizer({ open: true }) // Visualize the bundle after build
+  ],
+  base: "/Clarity-Canvas/",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    minify: 'terser', // Use terser for minification
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console logs
+      },
+    },
+  }
 })
